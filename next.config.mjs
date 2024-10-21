20d345fcc475795475b790959e310149b782a7e6
vinesk <vinesk@users.noreply.github.com>
2024-10-21 18:39:50 +0200

    Comment templates for projects

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["placehold.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
   },
 };
 
